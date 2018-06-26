fed5fd9fef9498661d1ab504357606ad7f954b3c
Carolina RC <caro.rmz315@gmail.com>
2018-06-26 12:02:32 -0500

    completed adding packages and creating backend server

--- server.js
+++ server.js
@@ -8,14 +8,28 @@
  // let yarn server know that all directed in local host 
 
 // since this is a mern app wil need to add stuff here
-
-// 
-
+const bodyParser = require("body-parser");
+var express = require("express");
 var mongoose = require("mongoose");
+var axios = require("axios");
+
+// express server 5000; 
+var PORT = process.env.PORT || 3001; 
+const app = express();
+
+// set extended to true to prvent strippign objecs
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("public"));
+};
 
 // connect to mongo DB
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 mongoose.Promise = Promise; 
 mongoose.connect(MONGODB_URI);
 
-// express server 5000; 
+// start server
+var server = app.listen(PORT, function() {
+    console.log("Server is listening on port : " + PORT)
+});
